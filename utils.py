fb784b7aa730310b1f469845b32d13bdd91a3c68
Clint Dimick <slyderc@me.com>
2020-02-28 22:20:16 -0800

    new game_log function

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 import configparser
 import argparse
 
@@ -10,16 +11,13 @@ def get_args():
     parser = argparse.ArgumentParser(
         description='PBR Arcade Beer Pong Game',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
     parser.add_argument('-c',
                         '--config',
                         type=str,
                         default='config.ini',
                         help='Config. file to use')
-
-    args = parser.parse_args()
-
-    return args
+    game_args = parser.parse_args()
+    return game_args
 
 
 def debug(msg, urgent=False):
@@ -27,6 +25,22 @@ def debug(msg, urgent=False):
         print('>> %s' % str(msg))
 
 
+def game_log(log_entry):
+    if config['defaults'].getboolean('game_log'):
+        log_time = time.asctime(time.localtime(time.time()))
+        log_string = (f'{log_time}: {log_entry}{nl}')
+        log_file = (f"{config['defaults']['game_path']}/game.log")
+
+        if os.path.isfile(log_file):
+            fh = open(f"{log_file}", "a")
+        else:
+            fh = open(f"{log_file}", "w")
+
+        fh.write(log_string)
+        fh.close()
+
+
+nl = '\n'
 args = get_args()
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 if not os.path.isfile(args.config):
